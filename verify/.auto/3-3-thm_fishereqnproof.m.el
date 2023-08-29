26bef763d768775fab4d34742541b941e29f2313
SamFrengley <stf32@cam.ac.uk>
2023-08-29 15:40:32 +0100

    Updated documentation and clarified some files

--- verify/.auto/3-3-thm_fishereqnproof.m.el
+++
@@ -1,4 +0,0 @@
-;;; This file was generated automatically.
-
-(magma-load-or-rescan (f-expand "../Z2_3_4/polys.m" magma-working-directory))
-(setq magma-completion-table (-union magma-completion-table '()))
--- verify/.auto/6-3-example.m.el
+++
@@ -1,5 +0,0 @@
-;;; This file was generated automatically.
-
-(magma-load-or-rescan (f-expand "../Z12-r/Z12equations.m" magma-working-directory))
-(magma-load-or-rescan (f-expand "../Z2_3_4/iscongruent.m" magma-working-directory))
-(setq magma-completion-table (-union magma-completion-table '()))
